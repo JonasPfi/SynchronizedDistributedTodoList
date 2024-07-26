f0980233c15f7236f7a31569142d8e83636e202d
JonasPfi <jonas.pfingstag@gmx.de>
2024-07-26 22:44:57 +0200

    Basic logging for socket-server

--- socket-server/socket-server.js
+++ socket-server/socket-server.js
@@ -5,13 +5,20 @@ const app = express();
 const PORT = process.env.PORT || 9090;
 const HOST = process.env.HOST || '0.0.0.0';
 
+// Middleware to log requests
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 app.get('/', (req, res) => {
-  res.send('<h1>Socket.io service</h1>');
+    res.send('<h1>Socket.io service</h1>');
 });
 
 // Start the actual server
-const expressServer = app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+const expressServer = app.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`);
+});
 
 // Define SocketIO
 const io = socketIO(expressServer, {
@@ -24,17 +31,18 @@ const io = socketIO(expressServer, {
 // Object to store which user has locked which todo
 const lockedTodos = new Map();
 
-io.on("connection", async (socket) => {
-    console.log("A user connected", socket.id);
+io.on("connection", (socket) => {
+    console.log(`A user connected with ID: ${socket.id}`);
 
     // Send all currently locked todos to the newly connected client
     socket.emit('initializeLocks', Array.from(lockedTodos.keys()));
 
     socket.on("disconnect", () => {
-        console.log("A user disconnected", socket.id);
+        console.log(`A user disconnected with ID: ${socket.id}`);
         // Remove all locks held by this user when they disconnect
         lockedTodos.forEach((userId, todoId) => {
             if (userId === socket.id) {
+                console.log(`User ${socket.id} released lock on todo ${todoId}`);
                 lockedTodos.delete(todoId);
                 socket.broadcast.emit('unlockElement', todoId);
             }
@@ -42,6 +50,7 @@ io.on("connection", async (socket) => {
     });
 
     socket.on("editTodo", (todoId) => {
+        console.log(`User ${socket.id} is locking todo ${todoId}`);
         // Save the lock
         lockedTodos.set(todoId, socket.id);
         // Notify other clients
@@ -49,12 +58,15 @@ io.on("connection", async (socket) => {
     });
 
     socket.on("unlockTodo", (todoId) => {
+        console.log(`User ${socket.id} is unlocking todo ${todoId}`);
         // Check if the current user has locked this todo
         if (lockedTodos.get(todoId) === socket.id) {
             // Remove the lock
             lockedTodos.delete(todoId);
             // Notify other clients
             socket.broadcast.emit('unlockElement', todoId);
+        } else {
+            console.log(`User ${socket.id} attempted to unlock todo ${todoId} but does not hold the lock`);
         }
     });
 });
